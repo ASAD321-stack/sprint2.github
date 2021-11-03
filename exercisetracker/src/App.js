76905408de6e1ff774aa3d7e7d2a71ed25c77577
ASAD321-stack <creativelu321@gmail.com>
2021-11-03 06:13:56 -0700

    changes

---
+++ exercisetracker/src/App.js
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+
+
+import Login from "./Login";
+import Signup from "./signup";
+import Addexercise from "./Addexercise";
+
+
+
+function App() {
+  return (
+    <div>
+    <Signup/>
+    </div>
+  );
+}
+
+export default App;
